41b39ca74c554e343935876c4daed6489f700460
Matthias <dellalibera40@gmail.com>
2023-08-17 20:37:47 +0200

    init

---
+++ ghiblilist/src/pages/tests/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const TestIndex:NextPage = () => {
+    return <h1>Quelle vie</h1>
+}
+
+export default TestIndex;
---
+++ ghiblilist/src/pages/tests/test.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Test:NextPage = () => {
+    return <h1>Coucou à tous</h1>
+}
+
+export default Test;
