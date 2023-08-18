794b0b93b1c19f3e89eeee4bb49dc947c1f69264
Matthias <dellalibera40@gmail.com>
2023-08-19 00:10:31 +0200

    zzz

--- ghibli-app/next.config.js
+++ ghibli-app/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    async headers() {
-        return [{
-            source: '/logos/favicon.png',
-            headers: [{
-                key: 'Cache-Control',
-                value: 'public, max-age=31536000, immutable',
-            }, ],
-        }, ];
-    },
 }
 
 module.exports = nextConfig
