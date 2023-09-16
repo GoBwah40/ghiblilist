c044fa9d6058d7045ae0ef23d914adf5b2023941
Matthias <dellalibera40@gmail.com>
2023-09-16 17:57:48 +0200

    alternate test config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,16 @@
 // jest.config.js
-module.exports = {
-  preset: "ts-jest/presets/js-with-babel",
-  testEnvironment: "jsdom",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-  moduleNameMapper: {
-    "\\.(css|scss|sass|less)$": "identity-obj-proxy",
-  },
-  setupFilesAfterEnv: [
-    "jest",
-    "@testing-library/jest-dom/extend-expect",
-    "@testing-library/react",
-  ],
+// module.exports = {
+//   preset: 'ts-jest/presets/js-with-babel',
+//   testEnvironment: 'jest-environment-jsdom',
+//   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+//   moduleNameMapper: {
+//     '\\.(css|scss|sass|less)$': 'identity-obj-proxy',
+//   },
+//   setupFilesAfterEnv: ['jest', '@testing-library/jest-dom/extend-expect', '@testing-library/react'],
+// };
+const config = {
+  verbose: true,
+  preset: 'ts-jest/presets/js-with-babel',
 };
+
+module.exports = config;
